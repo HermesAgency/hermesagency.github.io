676c7426dc1dc8a044bba6f1722b2d81264c3660
maurodev <mauroesandes@gmail.com>
2024-02-17 00:25:52 -0300

    feat(baseConfig.js):add arrow entry animation in mobile

--- src/scripts/baseConfig.js
+++ src/scripts/baseConfig.js
@@ -21,6 +21,7 @@ const mm = gsap.matchMedia()
 //Mobile Config
 mm.add('(max-width: 768px)', () => {
     // this setup code only runs when viewport is at least 768px wide
+    //Preloader animation
     tl.to('#text', {
         duration: 4,
         ease: 'none',
@@ -85,12 +86,50 @@ mm.add('(max-width: 768px)', () => {
         duration: 0.8,
         ease: 'power4.inOut',
         translateX: '270%',
+        onComplete: () => {
+            lenis.scrollTo(0, { immediate: true })
+        },
     })
 
     tl.to('#loader-container', {
         opacity: 0,
         display: 'none',
     })
+
+    //Hero section Entry animation
+    tl.fromTo(
+        '#hero-arrow-mobile',
+        {
+            opacity: 0,
+            scale: 0.2,
+            'stroke-dashoffset': '25%',
+            // 'stroke-dasharray': '0 32%',
+            // 'stroke-width': 0,
+        },
+        {
+            opacity: 1,
+            scale: 1,
+            transformOrigin: 'top right',
+            duration: 0.8,
+            ease: 'power3.inOut',
+            // 'stroke-width': 3,
+            'stroke-dashoffset': '-25%',
+            // 'stroke-dasharray': '32% 0',
+        }
+    )
+
+    tl.fromTo(
+        '#hero-content',
+        {
+            opacity: 0,
+            y: '50%',
+        },
+        {
+            opacity: 1,
+            translateY: '0%',
+            stagger: 0.1,
+        }
+    )
 })
 //Desktop Config
 mm.add('(min-width: 768px)', () => {
