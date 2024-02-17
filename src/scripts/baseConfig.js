c5c925b66f79a2946697b6e271e38ccd26b70795
maurodev <mauroesandes@gmail.com>
2024-02-16 23:28:27 -0300

    feat!(baseConfig.js):added pre-loader responsive animation

--- src/scripts/baseConfig.js
+++ src/scripts/baseConfig.js
@@ -12,3 +12,126 @@ gsap.ticker.add((time) => {
 })
 
 gsap.ticker.lagSmoothing(0)
+
+// PRELOADER ANIMATION
+const tl = gsap.timeline()
+
+const mm = gsap.matchMedia()
+
+//Mobile Config
+mm.add('(max-width: 768px)', () => {
+    // this setup code only runs when viewport is at least 768px wide
+    tl.from('svg#preloader', {
+        width: '309px',
+        height: '229px',
+    })
+    tl.to('#text', {
+        duration: 4,
+        ease: 'none',
+        keyframes: {
+            '0%': {
+                opacity: 1,
+                fill: 'transparent',
+                stroke: '#000',
+                'stroke-width': 3,
+                'stroke-dashoffset': '25%',
+                'stroke-dasharray': '0 32%',
+            },
+            '50%': {
+                fill: 'transparent',
+                stroke: '#000',
+                'stroke-width': 3,
+            },
+            '70%, 100%': {
+                fill: '#000',
+                stroke: 'transparent',
+                'stroke-width': '0',
+                'stroke-dashoffset': '-25%',
+                'stroke-dasharray': '32% 0',
+            },
+        },
+    })
+
+    tl.to(
+        '#wing',
+        {
+            duration: 4,
+            ease: 'none',
+            keyframes: {
+                '0%': {
+                    opacity: 1,
+                    fill: 'transparent',
+                    stroke: '#000',
+                    'stroke-width': 3,
+                    'stroke-dashoffset': '25%',
+                    'stroke-dasharray': '0 32%',
+                },
+                '50%': {
+                    fill: 'transparent',
+                    stroke: '#000',
+                    'stroke-width': 3,
+                },
+                '70%, 100%': {
+                    fill: '#000',
+                    stroke: 'transparent',
+                    'stroke-width': '0',
+                    'stroke-dashoffset': '-25%',
+                    'stroke-dasharray': '32% 0',
+                },
+            },
+        },
+        '<'
+    )
+
+    tl.to('#preloader', {
+        delay: 0.5,
+        scale: 80,
+        duration: 0.8,
+        ease: 'power4.inOut',
+        translateX: '270%',
+    })
+
+    tl.to('#loader-container', {
+        opacity: 0,
+        display: 'none',
+    })
+})
+//Desktop Config
+mm.add('(min-width: 768px)', () => {
+    // this setup code only runs when viewport is at least 768px wide
+    tl.to('#preloader', {
+        delay: 2,
+        scale: 90,
+        duration: 1.2,
+        translateX: '-100%',
+    })
+
+    tl.to('#loader-container', {
+        opacity: 0,
+        display: 'none',
+    })
+})
+
+// tl.to(
+//   "#wing",
+//   {
+//     opacity: 0,
+//   },
+//   "<"
+// );
+// tl.to(
+//   "#wing",
+//   {
+//     opacity: 0,
+//   },
+//   "<"
+// );
+// tl.to(
+//   "#preloader",
+//   {
+//     fill: "#030104",
+//     duration: 0,
+//     delay: 0.2,
+//   },
+//   "<"
+// );
