1dd65f3cb0edb77d9927c3ad5279f17fa62b4a88
maurodev <mauroesandes@gmail.com>
2024-02-19 22:27:20 -0300

    feat(aboutUs.js):finished mobile animation

--- src/scripts/aboutUs.js
+++ src/scripts/aboutUs.js
@@ -4,12 +4,14 @@ import gsap from 'gsap'
 gsap.registerPlugin(ScrollTrigger)
 const mm = gsap.matchMedia()
 const aboutCards = gsap.utils.toArray('.about__card')
+const metricCircles = gsap.utils.toArray('.metric__circle')
 const aboutHeroImg = document.querySelector('#about-us-image')
 const aboutArrowMobile = document.querySelector('#about-us-arrow-mobile')
 const aboutSectionTitle = document.querySelector('#about-us-title')
 const aboutSectionText = document.querySelector('#about-us-text')
 
 mm.add('(max-width: 767px)', () => {
+    //Function to add animation to card
     function addAnimationToCard(card) {
         let cardImg = card.querySelector('.about__card__image')
         let cardTitle = card.querySelector('.about__card__title')
@@ -18,7 +20,6 @@ mm.add('(max-width: 767px)', () => {
             scrollTrigger: {
                 trigger: card,
                 start: 'top 90%',
-                markers: true,
             },
         })
         //Card Image Entry
@@ -34,11 +35,10 @@ mm.add('(max-width: 767px)', () => {
                 translateX: '0',
                 translateY: '0',
                 duration: 1,
-                onStart: () => console.log(`animacion iniciada card `),
             }
         )
 
-        //Card text Entry
+        //Card title Entry
         cardTL.fromTo(
             cardTitle,
             {
@@ -48,11 +48,59 @@ mm.add('(max-width: 767px)', () => {
             {
                 opacity: 1,
                 translateX: '0',
+                ease: 'power3.inOut',
                 duration: 0.8,
-            }
+            },
+            '>-0.4'
         )
+        //Card text entry
         cardTL.fromTo(
             cardText,
+            {
+                opacity: 0,
+                translateX: '-30%',
+            },
+            {
+                opacity: 1,
+                translateX: '0',
+                ease: 'power3.inOut',
+                duration: 0.8,
+            },
+            '<+=0.2'
+        )
+    }
+
+    //Function to add animation to metric circles
+    function createMetricCirclesEntryAnimation(metrics) {
+        const metricsContainer = document.querySelector('#metrics__container')
+        const metricsTitle = document.querySelector('.metric__title')
+        const metricsText = document.querySelector('.metric__text')
+        const aboutTL2 = gsap.timeline({
+            scrollTrigger: {
+                trigger: metricsContainer,
+                start: 'top 90%',
+            },
+        })
+        //add animation to metrics
+        metrics.forEach((metric) => {
+            aboutTL2.fromTo(
+                metric,
+                {
+                    opacity: 0,
+                    translateY: '50%',
+                },
+                {
+                    opacity: 1,
+                    translateY: '0',
+                    ease: 'power3.inOut',
+                    duration: 0.4,
+                },
+                '>-0.2'
+            )
+        })
+        //Metric title entry
+        aboutTL2.fromTo(
+            metricsTitle,
             {
                 opacity: 0,
                 translateX: '-100%',
@@ -60,18 +108,35 @@ mm.add('(max-width: 767px)', () => {
             {
                 opacity: 1,
                 translateX: '0',
+                ease: 'power3.inOut',
                 duration: 0.8,
             }
         )
+        //Metric Text entry
+        aboutTL2.fromTo(
+            metricsText,
+            {
+                opacity: 0,
+                translateX: '-30%',
+            },
+            {
+                opacity: 1,
+                translateX: '0',
+                ease: 'power3.inOut',
+                duration: 0.8,
+            },
+            '>-0.4'
+        )
     }
 
+    //create part1 timeline
     const aboutTL1 = gsap.timeline({
         scrollTrigger: {
             trigger: aboutHeroImg,
             start: 'top 90%',
-            markers: true,
         },
     })
+    //Section Image entry
     aboutTL1.fromTo(
         aboutHeroImg,
         {
@@ -85,6 +150,7 @@ mm.add('(max-width: 767px)', () => {
             translateY: '0',
         }
     )
+    //Section arrow entry
     aboutTL1.fromTo(
         aboutArrowMobile,
         {
@@ -99,6 +165,7 @@ mm.add('(max-width: 767px)', () => {
             ease: 'power3.inOut',
         }
     )
+    //Section title entry
     aboutTL1.fromTo(
         aboutSectionTitle,
         {
@@ -111,20 +178,20 @@ mm.add('(max-width: 767px)', () => {
             duration: 0.8,
         }
     )
+    //Section Text Entry
     aboutTL1.fromTo(
         aboutSectionText,
         {
             opacity: 0,
-            translateX: '-100%',
         },
         {
             opacity: 1,
-            translateX: '0',
             duration: 0.8,
         }
     )
-
+    //add animation to Card in DomContentLoaded Event
     window.addEventListener('DOMContentLoaded', () => {
         aboutCards.forEach((card) => addAnimationToCard(card))
+        createMetricCirclesEntryAnimation(metricCircles)
     })
 })
