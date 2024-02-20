8a7819db6fd8f7eebef6a2108327a1cc68ec6986
maurodev <mauroesandes@gmail.com>
2024-02-20 14:47:41 -0300

    feat(ourServices.js):added tablet animations

--- src/scripts/ourServices.js
+++ src/scripts/ourServices.js
@@ -24,6 +24,8 @@ servTL.to(servicesSection, {
     transformOrigin: 'bottom left',
     duration: 0.8,
 })
+
+//Mobile animation
 mm.add('(max-width:767px)', () => {
     //Arrow Entry
     servTL.fromTo(
@@ -39,6 +41,55 @@ mm.add('(max-width:767px)', () => {
             transformOrigin: 'top right',
             duration: 0.5,
             ease: 'power3.inOut',
+            'stroke-dashoffset': '-25%',
+        }
+    )
+    //Title Entry
+    servTL.fromTo(
+        entryTitle,
+        {
+            opacity: 0,
+            translateX: '-100%',
+        },
+        {
+            opacity: 1,
+            translateX: '0',
+            duration: 0.5,
+            ease: 'power3.inOut',
+        }
+    )
+
+    servTL.fromTo(
+        serviceCardsContainer,
+        {
+            opacity: 0,
+            translateY: '100%',
+        },
+        {
+            opacity: 1,
+            translateY: '0',
+            duration: 0.5,
+            ease: 'power2.inOut',
+        }
+    )
+    //
+})
+//Tablet animation
+mm.add('(min-width:768px)', () => {
+    //Arrow Entry
+    servTL.fromTo(
+        entryArrow,
+        {
+            opacity: 0,
+            scale: 0.2,
+            'stroke-dashoffset': '25%',
+        },
+        {
+            opacity: 1,
+            scale: 1,
+            transformOrigin: 'top right',
+            duration: 0.5,
+            ease: 'power4.out',
             'stroke-dashoffset': '-25%',
         }
     )
