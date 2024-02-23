7064db58a466e9d556ec51890550a2ac588889a4
maurodev <mauroesandes@gmail.com>
2024-02-23 18:13:57 -0300

    feat(formSelectSwitch.js):create script to handle form service select switch on anchor click

---
+++ src/scripts/formSelectSwitch.js
@@ -0,0 +1,27 @@
+const selectInput = document.getElementById('serviceTypeInput')
+function changeSelectOption(value) {
+    selectInput.value = value
+}
+
+const pwaLinks = document.querySelectorAll('a.pwa__service')
+const ecommerceLinks = document.querySelectorAll('a.ecommerce__service')
+const landingLinks = document.querySelectorAll('a.landing__service')
+const advisoryLinks = document.querySelectorAll('.advisory__service')
+
+//create array with Results
+
+pwaLinks.forEach((link) => {
+    link.addEventListener('click', () => changeSelectOption('pwa'))
+})
+
+ecommerceLinks.forEach((link) => {
+    link.addEventListener('click', () => changeSelectOption('tienda virtual'))
+})
+
+landingLinks.forEach((link) => {
+    link.addEventListener('click', () => changeSelectOption('landing Page'))
+})
+
+advisoryLinks.forEach((link) => {
+    link.addEventListener('click', () => changeSelectOption('asesoria'))
+})
